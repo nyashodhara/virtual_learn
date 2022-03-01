b2f8d5487e7eeb276583566d22c574fd2f212b7e
Yashodhara <Yashodharagowdan@gmail.com>
2022-03-01 10:45:55 +0530

    changes

--- src/main/java/com/example/virtuallearn/Repository/InstructorRepository.java
+++ src/main/java/com/example/virtuallearn/Repository/InstructorRepository.java
@@ -2,6 +2,7 @@ package com.example.virtuallearn.Repository;
 
 import com.example.virtuallearn.Repository.Table.InstructorTable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,6 @@ import java.util.List;
 public interface InstructorRepository extends JpaRepository<InstructorTable,Long> {
     InstructorTable getByInstructor(String instructor);
 
+    @Query(value = "select * from instructor_tbl where id = :id", nativeQuery = true)
     List<InstructorTable> findByInstructorId(long id);
 }
